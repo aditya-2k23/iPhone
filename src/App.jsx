0f2aff8a1f6b6ea520251096dae1ff9bdbed5e86
Aditya <holaaditya123@gmail.com>
2024-06-16 17:20:46 +0530

    files setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex w-full h-[100vh] justify-center items-center bg-black">
+      <h1 className="text-5xl font-bold text-white underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
