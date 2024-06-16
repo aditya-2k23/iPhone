5ad0cc9366711e13a7f8ad0cc52751ed9aad5523
Aditya <131287474+aditya-2k23@users.noreply.github.com>
2024-06-16 12:26:44 +0000

    all file setup done ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div className="flex w-full h-[100vh] justify-center items-center bg-black">
-      <h1 className="text-5xl font-bold text-white underline">Hello world!</h1>
-    </div>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   );
 };
 
