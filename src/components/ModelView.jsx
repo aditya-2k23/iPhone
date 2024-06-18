037d91f402f2ec14c7efd288557bd8e3abe0f5c4
Aditya <holaaditya123@gmail.com>
2024-06-18 11:49:58 +0530

    Model.jsx done for the 3D model of iPhone

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,61 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+import Lights from "./Lights";
+import { Suspense } from "react";
+import IPhone from "./IPhone";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full ${index === 2} ? 'right-[-100%]' : ''`}
+    >
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new ThreeMFLoader.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState()}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large'`}
+        position={[0, 0, 0]}
+      >
+        <Suspense
+          fallback={
+            <Html>
+              <div>Loading</div>
+            </Html>
+          }
+        >
+          <IPhone />
+        </Suspense>
+      </group>
+    </View>
+  );
 };
 
 export default ModelView;
